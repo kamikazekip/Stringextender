6ef600b61b5242a4678e061130a00061de3308ad
Erik Brandsma <erik.brandsma@outlook.com>
2016-04-29 13:34:22 +0200

    Iets

---
+++ stringextender-lib/src/main/java/mma/afdelingapps/com/stringextender/ExtenderClass.java
@@ -0,0 +1,10 @@
+package mma.afdelingapps.com.stringextender;
+
+/**
+ * Created by Erik on 29-Apr-16.
+ */
+public class ExtenderClass {
+    public static String extend(){
+        return "extend";
+    }
+}
